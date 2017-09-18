bceb59cdb24978d75637997fcd9474fbad31a012
allen_chen <wkchen8337@gmail.com>
2017-09-18 22:52:41 +0800

    20170918-c-pratice
    2-2-3(count_letter
    )

--- C_program/2-2-3(count_letter)/main.c
+++ C_program/2-2-3(count_letter)/main.c
@@ -3,6 +3,38 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    char s[50];
+    printf("叫块璣ゅ﹃: ");
+    scanf("%s",s);
+    printf("–ダ计秖:\n");
+    printf("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z\n");
+    int c[26] = {0};
+    int i=0;
+    int j=0;
+    int k=0;
+    int num=0;
+
+    //ダ传糶
+    for(i=0;s[i];i++)
+    {
+        if(s[i] >= 'A' && s[i] <= 'Z')
+        {
+            s[i] = s[i] - 'A' + 'a';  // 糶ASCII CODE + 32 跑糶
+        }
+    }
+
+    //参璸ダ计秖
+    for(j=0;s[j];j++)
+    {
+        if(s[j] >= 'a' && s[j] <= 'z')
+        num = s[j] - 'a';
+        c[num] = c[num] + 1;
+    }
+
+    for(k=0;c[k]<=26;k++)
+    {
+        printf("%d ",c[k]);
+    }
+
     return 0;
 }
